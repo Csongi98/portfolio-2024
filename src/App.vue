b3bc980fdfbd3a8379ea85fc0b6c8f595a69361b
Csongor Karácsonyi <karacsonyi.csongor@gmail.com>
2025-01-13 23:45:30 +0100

    Safari warning

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
 	<div>
-		<LoadingScreen v-if="loading" />
+		<div v-if="isSafari" class="safari-warning">
+			<div class="safari-message">
+				<h1>Safari Version in Development</h1>
+				<p>Please use another browser for the best experience.</p>
+			</div>
+		</div>
 		<div v-else>
-			<div class="relative elementbg">
-				<img
-					class="absolute top-0 w-full"
-					src="/assets/images/5.png"
-					alt="Profile Image"
-				/>
-			</div>
-			<div class="appBody">
-				<Navbar></Navbar>
-				<Intro></Intro>
-				<Showcase></Showcase>
-				<AboutMe></AboutMe>
-				<BackToTop></BackToTop>
-				<Footer></Footer>
-			</div>
-			<div class="relative">
-				<img
-					class="absolute bottom-0 -z-10 w-full"
-					src="/assets/images/5circle.png"
-					alt="Profile Image"
-				/>
+			<LoadingScreen v-if="loading" />
+			<div v-else>
+				<div class="relative elementbg">
+					<img
+						class="absolute top-0 w-full"
+						src="/assets/images/5.png"
+						alt="Profile Image"
+					/>
+				</div>
+				<div class="appBody">
+					<Navbar></Navbar>
+					<Intro></Intro>
+					<Showcase></Showcase>
+					<AboutMe></AboutMe>
+					<BackToTop></BackToTop>
+					<Footer></Footer>
+				</div>
+				<div class="relative">
+					<img
+						class="absolute bottom-0 -z-10 w-full"
+						src="/assets/images/5circle.png"
+						alt="Profile Image"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -39,12 +47,16 @@ import BackToTop from "./components/BackToTop.vue";
 import Intro from "./components/Intro.vue";
 
 const loading = ref(true);
+const isSafari = ref(false);
 
-console.log("Loading state:", loading.value);
+const detectSafari = () => {
+	const ua = navigator.userAgent.toLowerCase();
+	isSafari.value =
+		ua.includes("safari") && !ua.includes("chrome") && !ua.includes("firefox");
+};
 
 const loadImages = async () => {
 	const images = Array.from(document.images);
-	console.log("Images to load:", document.images);
 	await Promise.all(
 		images.map((img) =>
 			img.complete
@@ -70,4 +82,27 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.safari-warning {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+	background-color: white;
+}
+
+.safari-message {
+	text-align: center;
+	color: #333;
+}
+
+.safari-message h1 {
+	font-size: 2rem;
+	font-weight: bold;
+	margin-bottom: 1rem;
+}
+
+.safari-message p {
+	font-size: 1rem;
+}
+</style>
