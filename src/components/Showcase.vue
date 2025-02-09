94d6298be0878675314b3daf500a9547d83bf009
Csongor Karácsonyi <karacsonyi.csongor@gmail.com>
2025-02-09 14:38:17 +0100

    Safari fix

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -9,21 +9,21 @@
 			<div class="grid gap-4">
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg z-10"
+						class="h-full max-w-full rounded-lg z-10 object-cover"
 						src="/assets/images/infpos.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/magazin.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/model2.jpg"
 						alt=""
 					/>
@@ -32,21 +32,21 @@
 			<div class="grid gap-4">
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/nvjegy.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/poster1.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/plax.jpg"
 						alt=""
 					/>
@@ -55,21 +55,21 @@
 			<div class="grid gap-4">
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/rajz3.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/kez1.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/model.png"
 						alt=""
 					/>
@@ -78,21 +78,21 @@
 			<div class="grid gap-4">
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/products.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/StoreSign.jpg"
 						alt=""
 					/>
 				</div>
 				<div>
 					<img
-						class="h-full max-w-full rounded-lg"
+						class="h-full max-w-full rounded-lg object-cover"
 						src="/assets/images/tattooplak.jpg"
 						alt=""
 					/>
